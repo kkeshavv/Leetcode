472e14472b8946f5f0838124c626ed241aeac514
keshav <112716732+kkeshavv@users.noreply.github.com>
2024-10-07 02:05:46 +0530

    Time: 5 ms (8.72%), Space: 8.6 MB (35.32%) - LeetHub

--- 1923-sentence-similarity-iii/1923-sentence-similarity-iii.cpp
+++ 1923-sentence-similarity-iii/1923-sentence-similarity-iii.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
     bool areSentencesSimilar(string s1, string s2) {
-       if(s1.length() < s2.length()){
-        swap(s1,s2);
-       } 
+        if(s1.size() < s2.size()){
+            swap(s1,s2);
+        }
 
-       vector<string> vec1,vec2;
-       stringstream ss1(s1);
-       string token;
+        vector<string> vec1,vec2;
+        stringstream ss1(s1);
+        string token;
+        while(ss1 >> token){
+            vec1.push_back(token);
+        }
 
-       while(ss1 >> token){
-        vec1.push_back(token);
-       }
+        stringstream ss2(s2);
+        while(ss2 >> token){
+            vec2.push_back(token);
+        }
 
-       stringstream ss2(s2);
-       while(ss2 >> token){
-        vec2.push_back(token);
-       }
+        int i=0,j=vec1.size()-1;
+        int k=0,l=vec2.size()-1;
 
-       int i=0,j=vec1.size()-1;
-       int k=0,l=vec2.size()-1;
-
-       while(i<vec1.size() && k<vec2.size() && vec1[i]==vec2[k]){
-        i++;
-        k++;
-       }
-       while(l >=k  && vec1[j]==vec2[l]){
-        j--;
-        l--;
-       }
-
-       if(l<k){
-        return true;
-       }
-       else{
-        return false;
-       }
+        while(i < vec1.size() && k< vec2.size() && vec1[i]==vec2[k]){
+            i++;
+            k++;
+        }
+        while(l>=k && vec1[j]==vec2[l]){
+            j--;
+            l--;
+        }
+        if(l<k){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 };
