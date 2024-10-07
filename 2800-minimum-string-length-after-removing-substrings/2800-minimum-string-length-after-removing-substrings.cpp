68f2fb20ad67dc4badcb919e4fdc9a2cd1e12f49
keshav <112716732+kkeshavv@users.noreply.github.com>
2024-10-07 22:28:17 +0530

    Time: 0 ms (100%), Space: 11.7 MB (31.24%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -1,21 +1,51 @@
+// class Solution {
+// public:
+//     int minLength(string s) {
+//         stack<char> st;
+//         for(int i=0;i<s.length();i++){
+//             char ch=s[i];
+        
+//        if(!st.empty()) {
+//                 char top = st.top();
+//                 if((ch == 'B' && top == 'A') || (ch == 'D' && top == 'C')) {
+//                     st.pop();  
+//                 } else {
+//                     st.push(ch); 
+//                 }
+//              }
+//              else { 
+//                 st.push(ch); 
+//             }
+//         } 
+//         return st.size(); 
+//     }
+// };
+
+
 class Solution {
 public:
     int minLength(string s) {
-        stack<char> st;
-        for(int i=0;i<s.length();i++){
-            char ch=s[i];
-        
-       if(!st.empty()) {
-                char top = st.top();
-                if((ch == 'B' && top == 'A') || (ch == 'D' && top == 'C')) {
-                    st.pop();  
-                } else {
-                    st.push(ch); 
-                }
-            } else {
-                st.push(ch); 
+        stack<char> stack;
+
+        for (int i = 0; i < s.length(); i++) {
+            char cur_char = s[i];
+
+            if (stack.empty()) {
+                stack.push(cur_char);
+                continue;
+            }
+
+            if (cur_char == 'B' && stack.top() == 'A') {
+                stack.pop();
+            }
+            else if (cur_char == 'D' && stack.top() == 'C') {
+                stack.pop();
+            }
+            else {
+                stack.push(cur_char);
             }
         }
-        return st.size(); 
+
+        return stack.size();
     }
 };
