0da3e8545ad836604a14f9c129c0370e4cbecf43
keshav <112716732+kkeshavv@users.noreply.github.com>
2024-10-09 00:03:27 +0530

    Time: 136 ms (16.81%), Space: 35.9 MB (33.36%) - LeetHub

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,16 +1,37 @@
 class Solution {
- public:
-  int minSwaps(string s) {
-    // Cancel out all the matched pairs, then we'll be left with "]]]..[[[".
-    // The answer is ceil(the number of unmatched pairs / 2).
-    int unmatched = 0;
+public:
+    int minSwaps(string s) {
+        stack<char> st;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s[i];
 
-    for (const char c : s)
-      if (c == '[')
-        ++unmatched;
-      else if (unmatched > 0)  // c == ']' and there's a match.
-        --unmatched;
+            if (ch == '[') {
+                st.push(ch);
+            } 
+            else {
+                if (!st.empty() && st.top() == '[') {
+                    st.pop();
+                } 
+                else {
+                    st.push(ch);
+                }
+            }
+        }
 
-    return (unmatched + 1) / 2;
-  }
+        // Count unmatched opening and closing brackets
+        int a = 0, b = 0;
+        while (!st.empty()) {
+            if (st.top() == '[') {
+                b++;
+            } 
+            else {
+                a++;
+            }
+            st.pop();  // Pop the element to avoid infinite loop
+        }
+
+        // Calculate the minimum number of swaps
+        int ans = (a + 1) / 2 + (b + 1) / 2;
+        return ans/2;
+    }
 };
