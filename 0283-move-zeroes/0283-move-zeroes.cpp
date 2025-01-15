ed7f42374878740335a65e9381b639889a203253
keshav <112716732+kkeshavv@users.noreply.github.com>
2025-01-15 21:46:43 +0530

    Time: 0 ms (100%), Space: 23.9 MB (15.09%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
-    void moveZeroes(vector<int>& a) {
-        int j = -1;
-        int n = a.size();
-
-        // Find the first occurrence of zero
-        for (int i = 0; i < n; i++) {
-            if (a[i] == 0) {
-                j = i;
+    void moveZeroes(vector<int>& arr) {
+        int j=-1;
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            if(arr[i]==0){
+                j=i;
                 break;
             }
         }
-
-        // If there are no zeros, return early
-        if (j == -1) return;
-
-        // Move non-zero elements and swap them with zeros
-        for (int i = j + 1; i < n; i++) {
-            if (a[i] != 0) {
-                swap(a[i], a[j]);
+         if (j == -1) return;
+        for(int i = j + 1; i < n; i++){
+            if(arr[i]!=0){
+                swap(arr[i],arr[j]);
                 j++;
             }
         }
     }
-};
+}; 
