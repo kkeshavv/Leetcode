9f1806b158351f76b30ea69e7e4ad1b2dcb0e696
keshav <112716732+kkeshavv@users.noreply.github.com>
2025-01-19 10:40:29 +0530

    Time: 0 ms (100%), Space: 17.6 MB (17.14%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -2,40 +2,39 @@ class Solution {
 public:
     vector<int> searchRange(vector<int>& arr, int target) {
         vector<int> v = {-1, -1};
-        int n = arr.size();
-        int low = 0;
-        int high = n - 1;
+        int n=arr.size();
+        int low=0;
+        int high=n-1;
 
-        // Find the left boundary
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (arr[mid] < target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]<target){
+                low=mid+1;
             }
-            if (arr[mid] == target) {
-                v[0] = mid;
+            else {
+                high=mid-1;
+            }
+             if(arr[mid]==target){
+                v[0]=mid;
             }
         }
+        low=0;
+        high=n-1;
 
-        // Reset variables for right boundary search
-        low = 0;
-        high = n - 1;
-
-        // Find the right boundary
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (arr[mid] > target) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]>target){
+                high=mid-1;
             }
-            if (arr[mid] == target) {
-                v[1] = mid;
+            else {
+                low=mid+1;
+            }
+            if(arr[mid]==target){
+                v[1]=mid;
             }
         }
 
         return v;
+
     }
 };
