e6f54b7aab61b3904f3beca672fc2faf18a61e96
keshav <112716732+kkeshavv@users.noreply.github.com>
2025-01-20 22:55:51 +0530

    Time: 0 ms (100%), Space: 8.1 MB (56.69%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
+        bool answer =false;
         // Check if the lengths of s and goal are equal, otherwise rotation is impossible
         if (s.length() != goal.length()) {
             return false;
         }
 
         string s1 = s + s;
-        size_t found = s1.find(goal); // Store the result in a size_t variable
-    
-        // Check if goal is a substring of s1
-        if (found != string::npos) {
-            return true;
-        } else {
+        size_t pos = s1.find(goal);
+        
+        // Check if the substring was not found
+        if (pos == -1) {
             return false;
         }
+        return true;
+        
     }
 };
