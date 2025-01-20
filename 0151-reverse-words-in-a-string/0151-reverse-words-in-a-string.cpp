89d6df36cb1d06f57bdcbbe9576c8c137f29f69b
keshav <112716732+kkeshavv@users.noreply.github.com>
2025-01-20 19:04:46 +0530

    Time: 0 ms (100%), Space: 10.4 MB (58.93%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
     string reverseWords(string s) {
-    stringstream ss(s);  // Create a stringstream from the string
-    vector<string> words;  // Vector to store words
-    
-    string word;
-    
-    // Extract words from the stringstream and store them in the vector
-    while (ss >> word) {
-        words.push_back(word);
-    }
-    int n=words.size();
-    int low=0,high=n-1;
-    while(low<=high){
-        swap(words[low],words[high]);
-        low++;
-        high--;
-    }
-    string result="";
-    for(int i=0;i<n;i++){
-        result += words[i];
-        if (i != n - 1) {
-            result += " ";  // Add space between words
+        reverse(s.begin(),s.end());
+        int n=s.length();
+        
+        string ans="";
+        for(int i=0;i<n;i++){
+            string word="";
+            while(i<n &&s[i]!=' '){
+                word+=s[i];
+                i++;
+            }
+            reverse(word.begin(),word.end());
+            if(word.length()>0){
+                ans+=" "+word; 
+            }
         }
-    }
-    return result;
-
+        return ans.substr(1);
     }
 };
