2b31db7c70e59eadd62bc41aa437b8a89486889c
keshav <112716732+kkeshavv@users.noreply.github.com>
2025-01-24 18:56:54 +0530

    Time: 0 ms (100%), Space: 9.8 MB (93.99%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
-    void helper(vector<int>& arr, vector<int>& current, vector<vector<int>>& result, int n, int i) {
-        if (i == n) {
-            result.push_back(current); // Add the current subset to the result
-            return;
+    void getallsubsets(vector<int>& nums, vector<int>&ans,vector<vector<int>>&allsubsets,int i,int n){
+        if(i==n){ 
+        // store subsets;
+        allsubsets.push_back({ans});
+        return;
         }
+ 
+        // inclusion
+        ans.push_back(nums[i]);
+        getallsubsets(nums,ans,allsubsets,i+1,n);
 
-        // Include the current element
-        current.push_back(arr[i]);
-        helper(arr, current, result, n, i + 1);
+        // exclusion
+        ans.pop_back();
+        getallsubsets(nums,ans,allsubsets,i+1,n);
 
-        // Exclude the current element
-        current.pop_back();
-        helper(arr, current, result, n, i + 1);
+
     }
-
-    vector<vector<int>> subsets(vector<int>& arr) {
-        int n = arr.size();
-        vector<int> current;
-        vector<vector<int>> result;
-        helper(arr, current, result, n, 0); // Start the recursion
-        return result;
-    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>allsubsets;
+        vector<int>ans;
+        int i=0;
+        int n=nums.size();
+        getallsubsets(nums,ans,allsubsets,0,n);
+        return allsubsets;
+    }  
 };
