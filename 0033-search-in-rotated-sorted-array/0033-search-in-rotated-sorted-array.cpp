1a0b152cd71abc719d0db78f3d71da8007420fc3
keshav <112716732+kkeshavv@users.noreply.github.com>
2025-03-11 14:52:55 +0530

    Time: 0 ms (100%), Space: 15.4 MB (33.57%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
-    int search(vector<int>& arr, int k) {
-        int low = 0, high = arr.size() - 1;  // Use arr.size() to get the size of the array
-        while (low <= high) {
-            int mid = (low + high) / 2;
+    int search(vector<int>& arr, int target) {
+        int low=0;
+        int high=arr.size()-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]==target){
+                return mid;
+            }
+            // left part is sorted;
 
-            // If mid points to the target
-            if (arr[mid] == k) return mid;
-
-            // If left part is sorted
-            if (arr[low] <= arr[mid]) {
-                if (arr[low] <= k && k <= arr[mid]) {
-                    // Element exists in the left part
-                    high = mid - 1;
+            if(arr[low]<=arr[mid]){
+                if(arr[low]<=target && target<=arr[mid]){
+                    high=mid-1;
                 }
-                else {
-                    // Element does not exist in the left part
-                    low = mid + 1;
+                else{
+                    low=mid+1;
                 }
             }
-            else { // If right part is sorted
-                if (arr[mid] <= k && k <= arr[high]) {
-                    // Element exists in the right part
-                    low = mid + 1;
+            // right part is sorted;
+
+            else{
+                if(arr[mid]<=target && target<=arr[high]){
+                    low=mid+1;
                 }
-                else {
-                    // Element does not exist in the right part
-                    high = mid - 1;
+                else{
+                    high=mid-1;
                 }
             }
         }
-        return -1;  // Element not found
+        return -1;
     }
 };
