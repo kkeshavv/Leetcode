05822e64742dd5407c0d7c5f63bb1cfccca45b0c
keshav <112716732+kkeshavv@users.noreply.github.com>
2025-04-23 01:10:12 +0530

    Time: 4 ms (1.58%), Space: 31.5 MB (9.6%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -1,29 +1,21 @@
-// binary search using recursion 
-
 class Solution {
 public:
-    int helper(vector<int>& arr, int target,int low,int high){
-        if (low > high) {
-            return -1; // Target not found
-        }
-        int mid=(low+high)/2; 
-        if(arr[mid]==target){
-            return mid; 
-        }
-        else if(arr[mid]<target){
-            return helper(arr,target,mid+1,high);
-        }
-        else if(arr[mid]>target){
-            return helper(arr,target,low,mid-1);
-        }
-        else{
-            return -1; 
-        }
-
-    }
     int search(vector<int>& arr, int target) {
-         int low=0;
-        int high=arr.size()-1; 
-        return helper(arr,target,low,high);
-    }  
-}; 
+        int n=arr.size();
+        int l=0;
+        int r=n-1;
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(arr[mid]==target){
+                return mid;
+            }
+            else if(arr[mid]>target){
+                r--;
+            }
+            else{
+                l++;
+            }
+        }
+        return -1;
+    }
+};
