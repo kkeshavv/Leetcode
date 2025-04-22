9826f443f4371298d935900b62ecf590001e5a2a
keshav <112716732+kkeshavv@users.noreply.github.com>
2025-04-22 23:55:46 +0530

    Time: 0 ms (100%), Space: 12.2 MB (95.81%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,36 +1,66 @@
+// class Solution {
+// public:
+//     void merge(vector<int>& arr1, int m, vector<int>& arr2, int n) {
+//         // Temporary array to hold merged result
+//         vector<int> arr3(m + n); 
+//         int left = 0;
+//         int right = 0;
+//         int index = 0;
+
+//         // Merging arr1 and arr2 into arr3
+//         while (left < m && right < n) {
+//             if (arr1[left] < arr2[right]) {
+//                 arr3[index] = arr1[left];
+//                 left++;
+//             } else {
+//                 arr3[index] = arr2[right];
+//                 right++;
+//             }
+//             index++;
+//         }
+
+//         // Copy remaining elements from arr1, if any
+//         while (left < m) {
+//             arr3[index] = arr1[left];
+//             left++;
+//             index++;
+//         }
+
+//         // Copy remaining elements from arr2, if any
+//         while (right < n) {
+//             arr3[index] = arr2[right];
+//             right++;
+//             index++;
+//         }
+
+//         // Copy the merged result back into arr1
+//         for (int i = 0; i < m + n; i++) {
+//             arr1[i] = arr3[i];  // Copy the result into arr1 (no need to copy to arr2)
+//         }
+//     }
+// };
+
+
 class Solution {
 public:
     void merge(vector<int>& arr1, int m, vector<int>& arr2, int n) {
-       vector<int> arr3(m + n); 
-        int left=0;
+        int left=m-1;
         int right=0;
-        int index=0;
-        while(left<m && right<n){
-            if(arr1[left]<arr2[right]){
-                arr3[index]=arr1[left];
-                index++;
-                left++;
+        while(left>=0 && right<n){
+            if(arr1[left]>arr2[right]){
+                swap(arr1[left],arr2[right]);
+                left--;
+                right++;
             }
             else{
-                arr3[index]=arr2[right];
-                index++;
-                right++;
+                break;
             }
         }
-            while(left<m){
-                arr3[index]=arr1[left];
-                index++;
-                left++;
-            }
-            while(right<n){
-                arr3[index]=arr2[right];   
-                index++;
-                right++;
-            } 
-        
+        sort(arr1.begin(), arr1.begin() + m);
+        sort(arr2.begin(),arr2.end());
 
-        for (int i = 0; i < m + n; i++) {
-            arr1[i] = arr3[i];
+        for (int i = 0; i < n; i++) {
+            arr1[m + i] = arr2[i];
         }
     }
 };
