cbb4e43582bad88ade2ed5cd893d2df325920cd5
keshav <112716732+kkeshavv@users.noreply.github.com>
2025-04-24 22:28:06 +0530

    Time: 0 ms (100%), Space: 15.3 MB (33.82%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,43 +1,31 @@
 class Solution {
 public:
-    int search(vector<int>& arr, int target) {
-        int n=arr.size();
+     int search(vector<int>& arr, int k) {
+        int n=arr.size(); 
         int l=0;
         int r=n-1;
-        int pivot=-1;
-        for(int i=0;i<n-1;i++){
-            if(arr[i]>arr[i+1]){
-                pivot=i;
-                break;
-            }
-        }
-
-        if(pivot==-1){
-            l=0;
-            r=n-1;
-        }
-        else if(target>=arr[0]){
-            l=0;
-            r=pivot;
-        }
-        else{
-            l=pivot+1;
-            r=n-1;
-        }
-
         while(l<=r){
             int mid=(l+r)/2;
 
-            if(arr[mid]==target){
-                return mid;
-            }
-            if(arr[mid]<target){
-                l=mid+1;
-            }
-            else{
+            if (arr[mid] == k) return mid;
+
+            if(arr[l]<=arr[mid]){
+                if(arr[l]<=k && k <= arr[mid]){
                 r=mid-1;
             }
+            else{
+                l=mid+1;
+            }
+            }
+            else{
+                if(arr[mid]<=k && k<= arr[r]){
+                    l=mid+1;
+                }
+                else{
+                    r=mid-1;
+                }
+            }
         }
-    return -1;
+        return -1;
     }
 };
